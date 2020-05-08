4b81cc3f5b5a1f2a31d08ed97696fec2133574be
vijay0405 <uvijaysimha@gmail.com>
2020-05-08 23:58:34 +0530

    change body color based on tab selected

--- app.js
+++ app.js
@@ -12,5 +12,15 @@ tabs.forEach(clickedTab => {
 
         // Add the active class on the clicked tab
         clickedTab.classList.add('active');
+        console.log(clickedTab.classList);
+        if(clickedTab.classList.contains('pink')) {
+            document.getElementById('main-body').style.backgroundColor = 'pink'
+        } else if(clickedTab.classList.contains('purple')) {
+            document.getElementById('main-body').style.backgroundColor = 'purple'
+        } else if(clickedTab.classList.contains('yellow')) {
+            document.getElementById('main-body').style.backgroundColor = 'orange'
+        } else if(clickedTab.classList.contains('teal')) {
+            document.getElementById('main-body').style.backgroundColor = 'teal'
+        }
     });
 });
